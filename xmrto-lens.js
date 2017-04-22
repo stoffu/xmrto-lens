8f0f127480dd8cbedf9612c9d51aceea9e4d0462
stoffu <stoffu@protonmail.ch>
2017-04-22 20:55:06 +0900

    mostly done, enough of css hell :/

--- xmrto-lens.js
+++ xmrto-lens.js
@@ -39,17 +39,16 @@ function show_success(msg) {
     // when the enire transaction is complete bring up this page telling them
     // the transaction went successfully.
     $("#xmrto-lens-modal").html(
-        "<div class='ui-state-highlight ui-corner-all xmrto-success'>" +
-            "Success!<br><br>" +
-            "<span class='xmrto-success-text'>" + msg + "</span><br><br> " +
-            "<br><a href='#' class='xmrto-finish'>Finish</a>" +
+        "<div class='xmrto-success'>" +
+            "<div class='text-orange text-bold text-xlarge'>Success!</div>" +
+            "<div class='vspace-10'></div>" +
+            msg +
         "</div>"
     );
-    $("#xmrto-lens-modal").dialog("option", "buttons", []);
 }
 
 function show_status(msg) {
-    $("#xmrto-lens-modal .xmrto-status").html(msg);
+    $("#xmrto-lens-modal .xmrto-status").html("Status: <span class='text-white'>" + msg + "</span>");
 }
 
 var spinner = '<div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>';
@@ -60,6 +59,7 @@ function inject_modal() {
     if(already_injected) {
         return;
     }
+
     $('body').append(
     "<div id='xmrto-lens-modal'>" +
         "<div class='xmrto-panel-header'>" +
@@ -100,9 +100,14 @@ function inject_modal() {
                 return;
             }
             var ticks = 0;
-            var seconds_remaining = null;
+            var state;
+            var seconds_till_timeout;
             interval_id = setInterval(function() {
-                if(ticks % 8 == 0) {
+                if (seconds_till_timeout) {
+                    seconds_till_timeout--;
+                }
+                // cehck the satus every 5 seconds
+                if(ticks % 5 == 0) {
                     $.post("https://xmr.to/api/v1/xmr2btc/order_status_query/", { uuid: order_create_res.uuid } ).done(function(order_status_query_res) {
                         if (order_status_query_res.error) {
                             show_error(order_status_query_res.error_msg);
@@ -110,10 +115,8 @@ function inject_modal() {
                             return;
                         }
 
-                        var state                           = order_status_query_res.state;
                         var btc_num_confirmations           = order_status_query_res.btc_num_confirmations;
                         var btc_transaction_id              = order_status_query_res.btc_transaction_id;
-                        var seconds_till_timeout            = order_status_query_res.seconds_till_timeout;
                         var xmr_amount_total                = order_status_query_res.xmr_amount_total;
                         var xmr_amount_remaining            = order_status_query_res.xmr_amount_remaining;
                         var xmr_num_confirmations_remaining = order_status_query_res.xmr_num_confirmations_remaining;
@@ -121,117 +124,100 @@ function inject_modal() {
                         var xmr_required_amount             = order_status_query_res.xmr_required_amount;
                         var xmr_required_payment_id         = order_status_query_res.xmr_required_payment_id;
 
-                        seconds_remaining = state == "UNPAID" || state == "UNDERPAID" ? seconds_till_timeout : null;
+                        state                = order_status_query_res.state;
+                        seconds_till_timeout = order_status_query_res.seconds_till_timeout;
 
-                        var final_modal =
-                            "<div class='xmrto-panel-body'>" +
-                                "<div>Send <span class='xmrto-remaining-amount text-white'></span> to:<div>" +
-                                "<div class='text-white text-xsmall'>" + xmr_receiving_address + "</div>" + 
-                                "<div>with Payment ID <span class='text-white'>" + xmr_required_payment_id + "</span><div>" +
-                                "<div class='text-small text-bold text-orange'>Don't forget to attach the Payment ID!</div>" +
-                                "<div class='vspace-20'></div>" +
-                                "<div>The fund will be converted to <span class='text-white'>" + btc_amount + "</span> BTC and sent to </div>" +
-                                "<div class='text-white'>" + btc_dest_address + "</div>" +
-                                "<div class='vspace-10'></div>" +
-                                "<div>Order secret key:</div>" +
-                                "<div class='text-white'>" + order_create_res.uuid + "</div>" +
-                                "<div class='vspace-10'></div>" +
-                                "<div>CLI command:</div>" +
-                                "<div class='text-xxsmall'><textarea class='width-100' style='height:40px'>transfer 4 " + xmr_receiving_address + " " + xmr_amount_remaining + " " + xmr_required_payment_id + "</textarea></div>" +
-                                "<div class='vspace-10'></div>" +
-                                "<div>QR code:</div>" +
-                                "<div id='xmrto-qrcode'></div>" +
-                            "</div>" +
-                            "<div class='xmrto-status-outer'>" +
-                                "<div class='xmrto-status pull-left'></div>" +
-                                "<div class='xmrto-timer pull-right'></div>" +
-                            "</div>";
-                        $("#xmrto-lens-modal").html(final_modal);
+                        var panel_body =
+                        "<div class='xmrto-panel-body'>" +
+                            "<div>Send <span class='xmrto-remaining-amount text-white'></span> to:<div>" +
+                            "<div class='text-white text-xsmall'>" + xmr_receiving_address + "</div>" + 
+                            "<div>with Payment ID <span class='text-white'>" + xmr_required_payment_id + "</span><div>" +
+                            "<div class='text-small text-bold text-orange'>Don't forget to attach the Payment ID!</div>" +
+                            "<div class='vspace-20'></div>" +
+                            "<div>The fund will be converted to <span class='text-white'>" + btc_amount + "</span> BTC and sent to </div>" +
+                            "<div class='text-white'>" + btc_dest_address + "</div>" +
+                            "<div class='vspace-10'></div>" +
+                            "<div>Order secret key:</div>" +
+                            "<div class='text-white'>" + order_create_res.uuid + "</div>" +
+                            "<div class='vspace-10'></div>" +
+                            "<div>Command line:</div>" +
+                            "<div class='text-xxsmall'><textarea class='width-100' style='height:40px' onclick='this.select()'>transfer 4 " + xmr_receiving_address + " " + xmr_amount_remaining + " " + xmr_required_payment_id + "</textarea></div>" +
+                            "<div class='vspace-10'></div>" +
+                            "<div>QR code:</div>" +
+                            "<div id='xmrto-qrcode'></div>" +
+                        "</div>";
 
-                        var qrstring =
-                            "monero:" + xmr_receiving_address +
-                            "?tx_payment_id=" + xmr_required_payment_id +
-                            "&tx_amount=" + xmr_amount_remaining +
-                            "&recipient_name=XMR.TO";
-                        new QRCode(document.getElementById("xmrto-qrcode"), qrstring);
+                        var status_outer =
+                        "<div class='xmrto-status-outer'>" +
+                            "<div class='xmrto-status pull-left'></div>" +
+                            "<div class='xmrto-timer pull-right'></div>" +
+                        "</div>";
 
-                        if (state == "UNDERPAID") {
-                            $("#xmrto-lens-modal .xmrto-remaining-amount").text("remaining " + xmr_amount_remaining + " XMR (totaling " + xmr_amount_total + " XMR)");
+                        if (state == "UNPAID" || state == "UNDERPAID") {
+                            $("#xmrto-lens-modal").html(panel_body + status_outer);
+
+                            var qrstring =
+                                "monero:" + xmr_receiving_address +
+                                "?tx_payment_id=" + xmr_required_payment_id +
+                                "&tx_amount=" + xmr_amount_remaining +
+                                "&recipient_name=XMR.TO";
+                            new QRCode(document.getElementById("xmrto-qrcode"), qrstring);
+
+                            if (state == "UNDERPAID") {
+                                $("#xmrto-lens-modal .xmrto-remaining-amount").text("remaining " + xmr_amount_remaining + " XMR (totaling " + xmr_amount_total + " XMR)");
+                            } else {
+                                $("#xmrto-lens-modal .xmrto-remaining-amount").text(xmr_amount_remaining + " XMR");
+                            }
 
                         } else {
-                            $("#xmrto-lens-modal .xmrto-remaining-amount").text(xmr_amount_remaining + " XMR");
+                            // payment already received, so show the status only
+                            $("#xmrto-lens-modal").html(status_outer);
                         }
 
                         switch (state) {
                             case "TO_BE_CREATED":
-                                show_status("Status: order creation pending. " + spinner);
+                                show_status("Order creation pending " + spinner);
                                 break;
                             case "UNPAID":
-                                show_status("Status: Awaiting Your " + "Monero" + " " + spinner);
+                                show_status("Awaiting your Monero " + spinner);
                                 break;
                             case "UNDERPAID":
-                                show_status("Status: Awaiting Your " + "Monero" + " (underpaid) " + spinner);
+                                show_status("Awaiting your remaining Monero " + spinner);
                                 break;
                             case "PAID_UNCONFIRMED":
-                                show_status("Status: Payment Received, waiting for confirmation. " + spinner);
+                                show_status("Payment received! Waiting for confirmation " + spinner);
                                 break;
                             case "PAID":
-                                show_status("Status: Payment received and confirmed, now sending BTC. " + spinner);
+                                show_status("Payment received and confirmed! Now sending Bitcoin " + spinner);
                                 break;
                             case "BTC_SENT":
-                                show_success("<div class='xmrto-in-out'>" + xmr_required_amount + " XMR was converted to " + btc_amount + " BTC and sent to " + "<strong>" + btc_dest_address + "</strong></div>");
-                                clearInterval(interval_id);
+                                show_success(
+                                    "<div><span class='text-white'>" + xmr_required_amount + " XMR</span> was converted to <span class='text-white'>" + btc_amount + " BTC</span> and sent to</div>" +
+                                    "<div class='text-white'>" + btc_dest_address + "</div>" +
+                                    "<div class='vspace-10'></div>" +
+                                    "<div>Transaction ID:</div>" +
+                                    "<div class='text-white text-small'>" + btc_transaction_id + "</div>" +
+                                    "<div class='vspace-10'></div>" +
+                                    "<div>Number of confirmations: <span class='text-white'>" + btc_num_confirmations + "</span></div>"
+                                );
                                 return;
                             case "TIMED_OUT":
-                                show_error("order timed out before payment was complete");
+                                show_error("Order timed out before payment was complete");
                                 clearInterval(interval_id);
                                 return;
                             case "NOT_FOUND":
-                                show_error("order wasn’t found in system (never existed or was purged)");
+                                show_error("Order wasn’t found in system (never existed or was purged)");
                                 clearInterval(interval_id);
                                 return;
                         }
                     });
                 }
 
-                if (seconds_remaining )//|| expiration)
-                {
-                    seconds_remaining--;
-                    //var seconds = seconds_remaining ? seconds_remaining : ((expiration - new Date()) / 1000).toFixed(0);
-                    var seconds = seconds_remaining;
-                    var timeText = ""
-                    var sec = 0;
-                    if(seconds > 59)
-                    {
-                        var min = Math.floor(seconds / 60);
-                        sec = seconds - (min * 60);
-
-                        if(sec < 10)
-                        {
-                            sec = "0"+sec;
-                        }
-
-                        timeText = min+":"+sec;
-                    }
-                    else
-                    {
-                        if(seconds < 10)
-                        {
-                            sec = "0"+seconds;
-                        }
-
-                        timeText ="0:"+sec;
-                    }
-                    
-                    if(seconds > 0) {
-                        $("#xmrto-lens-modal .xmrto-timer").text(timeText + " until expiration");
-                    } else {
-                        show_error("Time Expired! Please try again.");
-                        clearInterval(interval_id);
-                        return;
-                    }
-                } else {
-                    $("#xmrto-lens-modal .xmrto-timer").text('');
+                if (state == "UNPAID" || state == "UNDERPAID") {
+                    var minutes = Math.floor(seconds_till_timeout / 60);
+                    var seconds = seconds_till_timeout - minutes * 60;
+                    var timeText = (minutes < 10 ? "0" : "") + minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+                    $("#xmrto-lens-modal .xmrto-timer").text("Expires in " + timeText);
                 }
 
                 ticks++;
@@ -239,32 +225,18 @@ function inject_modal() {
         });
     });
 
-            $('.xmrto-limit, .xmrto-rate').fadeIn();
-            var altcoin_symbol = "xmr"
-            var pair = "btc_" + altcoin_symbol;
-    
-            $("#xmrto-lens-modal .xmrto-min-limit").html(spinner);
-            $("#xmrto-lens-modal .xmrto-max-limit").html(spinner);
-            $("#xmrto-lens-modal .xmrto-rate").html(spinner);
-            
-            $('#xmrto-lens-modal .xmrto-more-options').show();
-    
-            $.get("https://xmr.to/api/v1/xmr2btc/order_parameter_query/", function(response) {
-                if(response.error) {
-                    show_error("XMR.TO API returned an error: " + response.error_msg);
-                    return;
-                }
-                $("#xmrto-lens-modal .xmrto-rate").text("1 XMR = " + response.price + " BTC");
-    
-                    var lower_limit = response.lower_limit;
-                    var upper_limit = response.upper_limit;
-    
-                    $("#xmrto-lens-modal .xmrto-min-limit").text(response.lower_limit + " BTC");
-                    $("#xmrto-lens-modal .xmrto-max-limit").text(response.upper_limit + " BTC");
-    
-            }).error(function(response) {
-                show_error("General Ajax failure");
-            });
+    $("#xmrto-lens-modal .xmrto-min-limit").html(spinner);
+    $("#xmrto-lens-modal .xmrto-max-limit").html(spinner);
+    $("#xmrto-lens-modal .xmrto-rate").html(spinner);
+    $.get("https://xmr.to/api/v1/xmr2btc/order_parameter_query/", function(response) {
+        if(response.error) {
+            show_error("XMR.TO API returned an error: " + response.error_msg);
+            return;
+        }
+        $("#xmrto-lens-modal .xmrto-min-limit").text(response.lower_limit + " BTC");
+        $("#xmrto-lens-modal .xmrto-max-limit").text(response.upper_limit + " BTC");
+        $("#xmrto-lens-modal .xmrto-rate").text("1 XMR = " + response.price + " BTC");
+    });
 
     already_injected = true; // only inject once
 }
@@ -272,11 +244,8 @@ function inject_modal() {
 icon_url = chrome.extension.getURL("lens_icon_12.png");
 
 function wrapMatchesInNode(textNode) {
-
     var temp = document.createElement('div');
-
-    temp.innerHTML = textNode.data.replace(btc_regex, '$&<a class="xmrto-lens-link" href="#" data-address="$&"><img title="Click to send altcoins to this BTC address" src="' + icon_url + '"></a> ');
-
+    temp.innerHTML = textNode.data.replace(btc_regex, '$&<a class="xmrto-lens-link" href="#" data-address="$&"><img title="Click to send Monero to this Bitcoin address" src="' + icon_url + '"></a> ');
     // temp.innerHTML is now:
     // "\n    This order's reference number is <a href="/order/RF83297">RF83297</a>.\n"
     // |_______________________________________|__________________________________|___|
@@ -294,24 +263,13 @@ function wrapMatchesInNode(textNode) {
 }
 
 inject_lens_icon(document.body);
-//document.body.addEventListener("DOMNodeInserted", function(event) { inject_lens_icon(event.target); }, false);
-//document.body.addEventListener("DOMCharacterDataModified", function(event) { inject_lens_icon(event.target); }, false);
 
 $(function() {
-    
     $("body").on("click", '.xmrto-lens-link', function(event) {
         // When the user clicks on one of the fox icons embedded on the page,
         // this function gets called which launches the modal.
         event.preventDefault();
         var address = $(this).data('address');
-        //chrome.runtime.sendMessage({clicked_address: address});
-
-
-
-
-
-
-
         $("#xmrto-lens-modal .xmrto-address").val(address);
         $("#xmrto-lens-modal").dialog({
             show: { effect: "fade", duration: 300 },
@@ -325,24 +283,5 @@ $(function() {
                 clearInterval(interval_id);
             },
         });
-
-        $("body").on("click", ".xmrto-finish", function (event) {
-            event.preventDefault();
-            $("#xmrto-lens-modal").dialog("close");
-        });
-        //show_success("induced success");
-        $('input[data-toggle="popover"]').focus(function(){
-
-        });
-        $('input[data-toggle="popover"]').on('blur', function(){
-           $('.xmrto-popover').fadeOut().remove();
-        }).on('focus', function(){
-              var width = $(this).width() + 10;
-            var content = $(this).attr('data-content');
-            $('<div role="tooltip" class="xmrto-popover right" id="popover83172" style="display: none; left:' + width + 'px;"><div class="arrow"></div><div class="popover-content">' + content + '</div></div>').insertAfter(this).fadeIn();
-            var popHeight = $('.xmrto-popover').height() + 5;
-            $('.xmrto-popover').css({top: '50%', 'margin-top': -popHeight / 2});
-        });
     });
-
 });
